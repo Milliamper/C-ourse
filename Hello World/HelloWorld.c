42fbaf9fbf40896a10ffb6208517c7121ceb62b8
milliamper <szalaimarci99@gmail.com>
2023-09-14 13:33:37 +0200

    Hello World

---
+++ Hello World/HelloWorld.c
@@ -0,0 +1,13 @@
+#include <stdio.h> // standard input/output library
+
+int main()
+{
+
+    /* escape sequence = \t : tab
+                         \n : new line
+                         \" : print quotes 
+    */
+
+    printf("Hello World");
+    return 0;
+}
